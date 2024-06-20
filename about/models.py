22318fb172462f88c81aa9f297bc29b4198f5e56
kalyani kommula <kalyanib.tech505@gmail.com>
2024-06-20 12:05:58 +0000

    added comment CRUD

--- about/models.py
+++ about/models.py
@@ -1,14 +1,22 @@
 from django.db import models
-from django.contrib.auth.models import User
-
-
 
 # Create your models here.
+
+
 class About(models.Model):
     title = models.CharField(max_length=200)
-    content = models.TextField()
     updated_on = models.DateTimeField(auto_now=True)
-
+    content = models.TextField()
 
     def __str__(self):
-       return self.title
+        return self.title
+
+
+class CollaborateRequest(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    message = models.TextField()
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Collaboration request from {self.name}"
